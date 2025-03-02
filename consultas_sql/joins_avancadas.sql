98787b37fdd1e4f7e046309bb501d4e4c2682038
Luís Gustavo dos Santos <Lgsantos@users.noreply.github.com>
2025-03-02 10:22:47 -0300

    Update joins_avancadas.sql
    Inclusão de subconsultas

--- consultas_sql/joins_avancadas.sql
+++ consultas_sql/joins_avancadas.sql
@@ -52,3 +52,46 @@ SELECT m.nome
 FROM Medico m JOIN MedicoClinica mc ON(m.cod = mc.medico_cod)
 JOIN Clinica c ON(mc.clinica_cod = c.cod)
 WHERE c.nome = 'ortocentro';
+
+--As consultas abaixo usam subconsulta:
+--1. Listar nomes das Clinicas que tem especialidade
+SELECT c.nome 
+FROM Clinica c
+WHERE c.cod  IN (SELECT ec.clinica_cod FROM EspecialidadeClinica ec);
+
+--2. Listar nomes dos médicos que não tem consulta
+SELECT m.nome 
+FROM Medico m 
+WHERE m.cod NOT IN (SELECT c.medico_cod FROM Consulta c);
+
+--3. Listar nomes dos médicos que atendem na Clinica ortocentro
+SELECT m.nome 
+FROM Medico m 
+WHERE m.cod IN (SELECT mc.medico_cod FROM MedicoClinica mc 
+WHERE mc.clinica_cod IN (SELECT c.cod FROM Clinica c WHERE c.nome = 'ortocentro'));
+
+--4. Criação da tabela Folha_pagto e inserção de dados:
+CREATE TABLE Folha_Pagto ( medico_cod int, consulta_cod int, valor float,
+primary key(medico_cod, consulta_cod),
+foreign key (medico_cod) references Medico(cod),
+foreign key (consulta_cod) references Consulta(cod));
+
+INSERT INTO Folha_Pagto VALUES (1,6,100), (1,2,150),(2,3,100), (2, 4, 150), (3,5,50);
+
+5.Listar nomes dos médicos que recebem valor da consulta maior que a média
+SELECT m.nome 
+FROM Medico m  
+WHERE m.cod IN (SELECT fp.medico_cod FROM Folha_Pagto fp 
+WHERE fp.valor > (SELECT AVG(fp2.valor) FROM Folha_Pagto fp2));
+
+6.Listar nome dos médidos que não estão na folha de pagto
+SELECT m.nome 
+FROM Medico m  
+WHERE m.cod NOT IN (SELECT fp.medico_cod FROM Folha_Pagto fp );
+
+Não usar subconsulta: Listar nome dos médicos e para aqueles que estão na folha de pagto, listar a média de salário
+SELECT m.nome , AVG(fp.valor) 
+FROM Medico m 
+LEFT JOIN Folha_Pagto fp ON (m.cod = fp.medico_cod)
+GROUP BY m.nome;
+
