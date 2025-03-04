4d16fcdb2e48c5b91146dfdfdd95d39c17f1c826
Luís Gustavo dos Santos <Lgsantos@users.noreply.github.com>
2025-03-04 09:10:43 -0300

    Create script_restauracao.sql

---
+++ automacao/script_restauracao.sql
@@ -0,0 +1,5 @@
+-- Se precisar restaurar um backup, use o seguinte comando no terminal:
+-- mysql -u root -p nome_do_banco < backup.sql
+
+-- Se o banco ainda não existir, primeiro crie-o no MySQL:
+-- CREATE DATABASE nome_do_banco;
